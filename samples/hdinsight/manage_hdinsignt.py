092a974d6854cc423f3aa1932a4ee899f1fb11cb
Yuchao Yan <yuchaoyan@microsoft.com>
2020-10-29 15:10:43 +0800

    sample

--- samples/hdinsight/manage_hdinsignt.py
+++ samples/hdinsight/manage_hdinsignt.py
@@ -9,7 +9,6 @@ import time
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.hdinsight import HDInsightManagementClient
 from azure.mgmt.resource import ResourceManagementClient
-from azure.mgmt.storage import StorageManagementClient
 
 
 # - other dependence -
@@ -20,8 +19,6 @@ def main():
     SUBSCRIPTION_ID = os.environ.get("SUBSCRIPTION_ID", None)
     TIME = str(time.time()).replace('.', '')
     GROUP_NAME = "testhdinsight" + TIME
-    STORAGE = "storage" + TIME
-    HDINSIGHT = "hdinsight" + TIME
 
     # Create client
     # # For other authentication approaches, please see: https://pypi.org/project/azure-identity/
@@ -29,10 +26,7 @@ def main():
         credential=DefaultAzureCredential(),
         subscription_id=SUBSCRIPTION_ID
     )
-    storage_client = StorageManagementClient(
-        credentials=DefaultAzureCredential(),
-        subscription_id=SUBSCRIPTION_ID
-    )
+
     hdinsight_client = HDInsightManagementClient(
         credential=DefaultAzureCredential(),
         subscription_id=SUBSCRIPTION_ID
@@ -46,54 +40,12 @@ def main():
         {"location": "eastus"}
     )
 
-    # Create storage
-    storage_client.storage_accounts.begin_create(
-        GROUP_NAME,
-        STORAGE,
-        {
-            "location": "eastus",
-            "sku": {'name': 'Standard_LRS'},
-            "kind": 'StorageV2',
-            'enable_https_traffic_only': True,
-        }
-    ).result()
-
     # - init depended resources -
     # - end -
 
     # Create hdinsight
-    hdinsight = hdinsight_client.hdinsights.begin_create(
-        GROUP_NAME,
-        HDINSIGHT,
-        {
-            # TODO: init resource body
-        }
-    ).result()
-    print("Create hdinsight:\n{}\n".format(hdinsight))
-
-    # Get hdinsight
-    hdinsight = hdinsight_client.hdinsights.get(
-        GROUP_NAME,
-        HDINSIGHT
-    )
-    print("Get hdinsight:\n{}\n".format(hdinsight))
-
-    # Update hdinsight
-    hdinsight = hdinsight_client.hdinsights.begin_update(
-        GROUP_NAME,
-        HDINSIGHT,
-        {
-            # TODO: init resource body
-        }
-    ).result()
-    print("Update hdinsight:\n{}\n".format(hdinsight))
-
-    # Delete hdinsight
-    hdinsight = hdinsight_client.hdinsights.begin_delete(
-        GROUP_NAME,
-        HDINSIGHT
-    ).result()
-    print("Delete hdinsight.\n")
+    hdinsight_client.operations.list()
+    print("operations:\n")
 
     # Delete Group
     resource_client.resource_groups.begin_delete(
