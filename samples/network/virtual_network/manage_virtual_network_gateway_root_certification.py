f605a0e1cb1dff8eb35cc8edc6359740d9e9b0a8
Zhenbiao Wei (WICRESOFT NORTH AMERICA LTD) <v-zhenbwei@microsoft.com>
2022-02-18 13:51:54 +0800

    add sample to gateway

---
+++ samples/network/virtual_network/manage_virtual_network_gateway_root_certification.py
@@ -0,0 +1,58 @@
+import os
+
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.network.models import VirtualNetworkGateway, VirtualNetworkGatewayIPConfiguration, SubResource, \
+    VirtualNetworkGatewaySku, VpnClientConfiguration, AddressSpace, VpnClientRootCertificate
+
+
+def main():
+
+    credentials = DefaultAzureCredential()
+    subscription = os.getenv('SUBSCRIPTION_ID')
+
+    nmclient = NetworkManagementClient(credentials, subscription)
+
+    GROUP_NAME = 'testgroupx'  # Resource groups name
+    VIRTUAL_NETWORK_NAME = 'virtualnetworkxx'
+    NEW_VIRTUAL_NETWORK_GATEWAY= 'newgateway'
+    SUBNET = 'GatewaySubnet'  # Must be `GatewaySubnet`
+    PUBLIC_IP_ADDRESS_NAME = 'publicipaddressxxxx'
+    IP_CONFIGURATION_NAME = 'default'
+    root_cert = "xxxxxxx"  # root certification
+
+    vn_para = VirtualNetworkGateway(
+        ip_configurations=[VirtualNetworkGatewayIPConfiguration(
+            private_ip_allocation_method="Dynamic",
+            subnet=SubResource(
+                id="/subscriptions/" + subscription + "/resourceGroups/" + GROUP_NAME + "/providers/Microsoft.Network/virtualNetworks/" + VIRTUAL_NETWORK_NAME + "/subnets/" + SUBNET + ""),
+            public_ip_address=SubResource(
+                id="/subscriptions/" + subscription + "/resourceGroups/" + GROUP_NAME + "/providers/Microsoft.Network/publicIPAddresses/" + PUBLIC_IP_ADDRESS_NAME + ""),
+            name=IP_CONFIGURATION_NAME
+        )],
+        gateway_type="Vpn",
+        vpn_type="RouteBased",
+        enable_bgp=False,
+        active=False,
+        enable_dns_forwarding=False,
+        sku=VirtualNetworkGatewaySku(name="VpnGw2", tier="VpnGw2"),
+        vpn_client_configuration=VpnClientConfiguration(
+            vpn_client_address_pool=AddressSpace(address_prefixes=["192.168.0.0/24"]),
+            vpn_authentication_types=['Certificate'],
+            vpn_client_root_certificates=[VpnClientRootCertificate(name="testCA", public_cert_data=root_cert)],
+            vpn_client_protocols=["OpenVPN"],
+        ),
+        vpn_gateway_generation="Generation2",
+        location="westus"
+    )
+
+    virtual_network_gateway = nmclient.virtual_network_gateways.begin_create_or_update(
+        GROUP_NAME,
+        NEW_VIRTUAL_NETWORK_GATEWAY,
+        vn_para
+    ).result()
+    print(virtual_network_gateway)
+
+
+if __name__ == '__main__':
+    main()
