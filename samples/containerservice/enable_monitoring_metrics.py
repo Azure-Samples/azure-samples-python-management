76407dc346217a7ad01f1329e530c0d1c67edaf9
ChenxiJiang333 <119990644+ChenxiJiang333@users.noreply.github.com>
2025-04-10 17:14:29 +0800

    Refactor monitoring metrics script and add templates

--- samples/containerservice/enable_monitoring_metrics.py
+++ samples/containerservice/enable_monitoring_metrics.py
@@ -12,11 +12,13 @@
 # - end -
 
 from azure.identity import DefaultAzureCredential
+from azure.core.rest import HttpRequest
 from azure.mgmt.containerservice import ContainerServiceClient
-from azure.mgmt.monitor import  MonitorManagementClient
+from azure.mgmt.monitor import MonitorManagementClient
 from azure.mgmt.alertsmanagement import AlertsManagementClient
 from dotenv import load_dotenv
-import os
+import os, json
+
 
 def main():
     load_dotenv()
@@ -71,11 +73,21 @@ def main():
             "properties": {
                 "dataCollectionEndpointId": data_collection_endpoint_id,
                 "dataSources": {
-                    "prometheusForwarder": [{"name": "PrometheusDataSource", "streams": ["Microsoft-PrometheusMetrics"], "labelIncludeFilter": {}}]
+                    "prometheusForwarder": [
+                        {
+                            "name": "PrometheusDataSource",
+                            "streams": ["Microsoft-PrometheusMetrics"],
+                            "labelIncludeFilter": {},
+                        }
+                    ]
                 },
                 "dataFlows": [{"destinations": ["MonitoringAccount1"], "streams": ["Microsoft-PrometheusMetrics"]}],
-                "description": "DCR description", 
-                "destinations": {"monitoringAccounts": [{"accountResourceId": azure_monitor_workspace_id, "name": "MonitoringAccount1"}]}
+                "description": "DCR description",
+                "destinations": {
+                    "monitoringAccounts": [
+                        {"accountResourceId": azure_monitor_workspace_id, "name": "MonitoringAccount1"}
+                    ]
+                },
             },
         },
     ).id
@@ -93,6 +105,30 @@ def main():
         },
     )
 
+    # Get rule templates
+    def get_recording_rules_template(i: int) -> list:
+        response = alert_management_client._send_request(
+            HttpRequest(
+                "GET",
+                f"{azure_monitor_workspace_id}/providers/microsoft.alertsmanagement/alertRuleRecommendations?api-version=2023-01-01-preview",
+            )
+        )
+        data = json.loads(response.text())
+        filtered_templates = [
+            template
+            for template in data.get("value", [])
+            if template.get("properties", {}).get("alertRuleType", "").lower()
+            == "microsoft.alertsmanagement/prometheusrulegroups"
+            and isinstance(template.get("properties", {}).get("rulesArmTemplate", {}).get("resources"), list)
+            and all(
+                isinstance(rule, dict) and "record" in rule and "expression" in rule
+                for resource in template["properties"]["rulesArmTemplate"]["resources"]
+                if resource.get("type", "").lower() == "microsoft.alertsmanagement/prometheusrulegroups"
+                for rule in resource.get("properties", {}).get("rules", [])
+            )
+        ]
+        return filtered_templates[i]["properties"]["rulesArmTemplate"]["resources"][0]["properties"]["rules"]
+
     # Create prometheus rule groups
     alert_management_client.prometheus_rule_groups.create_or_update(
         resource_group_name=RESOURCE_GROUP_NAME,
@@ -105,21 +141,8 @@ def main():
                 "enabled": True,
                 "clusterName": MANAGED_CLUSTERS_NAME,
                 "interval": "PT1M",
-                # Recommended rules template
-                "rules": [
-                    {"record": "instance:node_num_cpu:sum", "expression": "count without (cpu, mode) (  node_cpu_seconds_total{job=\"node\",mode=\"idle\"})"}, 
-                    {"record": "instance:node_cpu_utilisation:rate5m", "expression": "1 - avg without (cpu) (  sum without (mode) (rate(node_cpu_seconds_total{job=\"node\", mode=~\"idle|iowait|steal\"}[5m])))"}, 
-                    {"record": "instance:node_load1_per_cpu:ratio", "expression": "(  node_load1{job=\"node\"}/  instance:node_num_cpu:sum{job=\"node\"})"}, 
-                    {"record": "instance:node_memory_utilisation:ratio", "expression": "1 - (  (    node_memory_MemAvailable_bytes{job=\"node\"}    or    (      node_memory_Buffers_bytes{job=\"node\"}      +      node_memory_Cached_bytes{job=\"node\"}      +      node_memory_MemFree_bytes{job=\"node\"}      +      node_memory_Slab_bytes{job=\"node\"}    )  )/  node_memory_MemTotal_bytes{job=\"node\"})"}, 
-                    {"record": "instance:node_vmstat_pgmajfault:rate5m", "expression": "rate(node_vmstat_pgmajfault{job=\"node\"}[5m])"}, 
-                    {"record": "instance_device:node_disk_io_time_seconds:rate5m", "expression": "rate(node_disk_io_time_seconds_total{job=\"node\", device!=\"\"}[5m])"}, 
-                    {"record": "instance_device:node_disk_io_time_weighted_seconds:rate5m", "expression": "rate(node_disk_io_time_weighted_seconds_total{job=\"node\", device!=\"\"}[5m])"}, 
-                    {"record": "instance:node_network_receive_bytes_excluding_lo:rate5m", "expression": "sum without (device) (  rate(node_network_receive_bytes_total{job=\"node\", device!=\"lo\"}[5m]))"}, 
-                    {"record": "instance:node_network_transmit_bytes_excluding_lo:rate5m", "expression": "sum without (device) (  rate(node_network_transmit_bytes_total{job=\"node\", device!=\"lo\"}[5m]))"}, 
-                    {"record": "instance:node_network_receive_drop_excluding_lo:rate5m", "expression": "sum without (device) (  rate(node_network_receive_drop_total{job=\"node\", device!=\"lo\"}[5m]))"}, 
-                    {"record": "instance:node_network_transmit_drop_excluding_lo:rate5m", "expression": "sum without (device) (  rate(node_network_transmit_drop_total{job=\"node\", device!=\"lo\"}[5m]))"}
-                ]
-            }
+                "rules": get_recording_rules_template(0),
+            },
         },
     )
 
@@ -134,29 +157,8 @@ def main():
                 "enabled": True,
                 "clusterName": MANAGED_CLUSTERS_NAME,
                 "interval": "PT1M",
-                # Recommended rules template
-                "rules": [
-                    {"record": "node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate", "expression": "sum by (cluster, namespace, pod, container) (  irate(container_cpu_usage_seconds_total{job=\"cadvisor\", image!=\"\"}[5m])) * on (cluster, namespace, pod) group_left(node) topk by (cluster, namespace, pod) (  1, max by(cluster, namespace, pod, node) (kube_pod_info{node!=\"\"}))"}, 
-                    {"record": "node_namespace_pod_container:container_memory_working_set_bytes", "expression": "container_memory_working_set_bytes{job=\"cadvisor\", image!=\"\"}* on (namespace, pod) group_left(node) topk by(namespace, pod) (1,  max by(namespace, pod, node) (kube_pod_info{node!=\"\"}))"}, 
-                    {"record": "node_namespace_pod_container:container_memory_rss", "expression": "container_memory_rss{job=\"cadvisor\", image!=\"\"}* on (namespace, pod) group_left(node) topk by(namespace, pod) (1,  max by(namespace, pod, node) (kube_pod_info{node!=\"\"}))"}, 
-                    {"record": "node_namespace_pod_container:container_memory_cache", "expression": "container_memory_cache{job=\"cadvisor\", image!=\"\"}* on (namespace, pod) group_left(node) topk by(namespace, pod) (1,  max by(namespace, pod, node) (kube_pod_info{node!=\"\"}))"}, 
-                    {"record": "node_namespace_pod_container:container_memory_swap", "expression": "container_memory_swap{job=\"cadvisor\", image!=\"\"}* on (namespace, pod) group_left(node) topk by(namespace, pod) (1,  max by(namespace, pod, node) (kube_pod_info{node!=\"\"}))"}, 
-                    {"record": "cluster:namespace:pod_memory:active:kube_pod_container_resource_requests", "expression": "kube_pod_container_resource_requests{resource=\"memory\",job=\"kube-state-metrics\"}  * on (namespace, pod, cluster)group_left() max by (namespace, pod, cluster) (  (kube_pod_status_phase{phase=~\"Pending|Running\"} == 1))"}, 
-                    {"record": "namespace_memory:kube_pod_container_resource_requests:sum", "expression": "sum by (namespace, cluster) (    sum by (namespace, pod, cluster) (        max by (namespace, pod, container, cluster) (          kube_pod_container_resource_requests{resource=\"memory\",job=\"kube-state-metrics\"}        ) * on(namespace, pod, cluster) group_left() max by (namespace, pod, cluster) (          kube_pod_status_phase{phase=~\"Pending|Running\"} == 1        )    ))"}, 
-                    {"record": "cluster:namespace:pod_cpu:active:kube_pod_container_resource_requests", "expression": "kube_pod_container_resource_requests{resource=\"cpu\",job=\"kube-state-metrics\"}  * on (namespace, pod, cluster)group_left() max by (namespace, pod, cluster) (  (kube_pod_status_phase{phase=~\"Pending|Running\"} == 1))"}, 
-                    {"record": "namespace_cpu:kube_pod_container_resource_requests:sum", "expression": "sum by (namespace, cluster) (    sum by (namespace, pod, cluster) (        max by (namespace, pod, container, cluster) (          kube_pod_container_resource_requests{resource=\"cpu\",job=\"kube-state-metrics\"}        ) * on(namespace, pod, cluster) group_left() max by (namespace, pod, cluster) (          kube_pod_status_phase{phase=~\"Pending|Running\"} == 1        )    ))"}, 
-                    {"record": "cluster:namespace:pod_memory:active:kube_pod_container_resource_limits", "expression": "kube_pod_container_resource_limits{resource=\"memory\",job=\"kube-state-metrics\"}  * on (namespace, pod, cluster)group_left() max by (namespace, pod, cluster) (  (kube_pod_status_phase{phase=~\"Pending|Running\"} == 1))"}, 
-                    {"record": "namespace_memory:kube_pod_container_resource_limits:sum", "expression": "sum by (namespace, cluster) (    sum by (namespace, pod, cluster) (        max by (namespace, pod, container, cluster) (          kube_pod_container_resource_limits{resource=\"memory\",job=\"kube-state-metrics\"}        ) * on(namespace, pod, cluster) group_left() max by (namespace, pod, cluster) (          kube_pod_status_phase{phase=~\"Pending|Running\"} == 1        )    ))"}, 
-                    {"record": "cluster:namespace:pod_cpu:active:kube_pod_container_resource_limits", "expression": "kube_pod_container_resource_limits{resource=\"cpu\",job=\"kube-state-metrics\"}  * on (namespace, pod, cluster)group_left() max by (namespace, pod, cluster) ( (kube_pod_status_phase{phase=~\"Pending|Running\"} == 1) )"}, 
-                    {"record": "namespace_cpu:kube_pod_container_resource_limits:sum", "expression": "sum by (namespace, cluster) (    sum by (namespace, pod, cluster) (        max by (namespace, pod, container, cluster) (          kube_pod_container_resource_limits{resource=\"cpu\",job=\"kube-state-metrics\"}        ) * on(namespace, pod, cluster) group_left() max by (namespace, pod, cluster) (          kube_pod_status_phase{phase=~\"Pending|Running\"} == 1        )    ))"}, 
-                    {"record": "namespace_workload_pod:kube_pod_owner:relabel", "expression": "max by (cluster, namespace, workload, pod) (  label_replace(    label_replace(      kube_pod_owner{job=\"kube-state-metrics\", owner_kind=\"ReplicaSet\"},      \"replicaset\", \"$1\", \"owner_name\", \"(.*)\"    ) * on(replicaset, namespace) group_left(owner_name) topk by(replicaset, namespace) (      1, max by (replicaset, namespace, owner_name) (        kube_replicaset_owner{job=\"kube-state-metrics\"}      )    ),    \"workload\", \"$1\", \"owner_name\", \"(.*)\"  ))", "labels": {"workload_type": "deployment"}}, 
-                    {"record": "namespace_workload_pod:kube_pod_owner:relabel", "expression": "max by (cluster, namespace, workload, pod) (  label_replace(    kube_pod_owner{job=\"kube-state-metrics\", owner_kind=\"DaemonSet\"},    \"workload\", \"$1\", \"owner_name\", \"(.*)\"  ))", "labels": {"workload_type": "daemonset"}}, 
-                    {"record": "namespace_workload_pod:kube_pod_owner:relabel", "expression": "max by (cluster, namespace, workload, pod) (  label_replace(    kube_pod_owner{job=\"kube-state-metrics\", owner_kind=\"StatefulSet\"},    \"workload\", \"$1\", \"owner_name\", \"(.*)\"  ))", "labels": {"workload_type": "statefulset"}}, 
-                    {"record": "namespace_workload_pod:kube_pod_owner:relabel", "expression": "max by (cluster, namespace, workload, pod) (  label_replace(    kube_pod_owner{job=\"kube-state-metrics\", owner_kind=\"Job\"},    \"workload\", \"$1\", \"owner_name\", \"(.*)\"  ))", "labels": {"workload_type": "job"}}, 
-                    {"record": ":node_memory_MemAvailable_bytes:sum", "expression": "sum(  node_memory_MemAvailable_bytes{job=\"node\"} or  (    node_memory_Buffers_bytes{job=\"node\"} +    node_memory_Cached_bytes{job=\"node\"} +    node_memory_MemFree_bytes{job=\"node\"} +    node_memory_Slab_bytes{job=\"node\"}  )) by (cluster)"}, 
-                    {"record": "cluster:node_cpu:ratio_rate5m", "expression": "sum(rate(node_cpu_seconds_total{job=\"node\",mode!=\"idle\",mode!=\"iowait\",mode!=\"steal\"}[5m])) by (cluster) /count(sum(node_cpu_seconds_total{job=\"node\"}) by (cluster, instance, cpu)) by (cluster)"}
-                ]
-            }
+                "rules": get_recording_rules_template(1),
+            },
         },
     )
 
@@ -164,11 +166,9 @@ def main():
     managed_cluster = containerservice_client.managed_clusters.begin_create_or_update(
         resource_group_name=RESOURCE_GROUP_NAME,
         resource_name=MANAGED_CLUSTERS_NAME,
-        parameters={
-            "location": AZURE_LOCATION,
-            "azureMonitorProfile": {"metrics": {"enabled": True}}
-        }
+        parameters={"location": AZURE_LOCATION, "azureMonitorProfile": {"metrics": {"enabled": True}}},
     ).result()
+
 
 if __name__ == "__main__":
     main()
